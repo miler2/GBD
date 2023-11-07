908930bfad6925352dfea9aa7bf9e5a9448e65e0
miler2 <vegavegewtu@gmail.com>
2023-11-07 12:15:07 +0000

    He terminado la práctica

--- configuracion_1.2/configuracion.sh
+++ configuracion_1.2/configuracion.sh
@@ -12,14 +12,13 @@ cp my-custom.cnf /root/docker/mysql/conf.d/my-custom.cnf
 #Eliminamos el docker y lo ejecutamos de nuevo.
 docker stop mysql
 docker rm mysql
-#Ejecutamos el mysql.sh, volviendo a crear el docker para unir los dos archivos de configuración con este archivo .sh
+#Ejecutamos el mysql.sh, volviendo a crear el docker para unir los dos archivos de configuración usando este archivo .sh
 ./mysql.sh
 
-#Una vez hecho esto usaremos este comando para ver si hemos realizado correctamente los cambios
-echo "Aquí vemos si los cambios se han realizado correctamente, tiene que salir 250."
-echo "Si sale 150, es que no se ha realizado correctamente"
-echo "Ejecuta el siguiente comando desde la consola del docker para verificar los cambios"
-echo "sudo docker exec -it mysql bash"
-echo "mysql -u root -pMiler -e 'show global variables like "max_connections"';"
-#Este comando no me funciona (preguntar en IAW)
-#docker exec -i mysql bash < mysql -u root -pMiler -e 'show global variables like "max_connections"';
+
+#Creamos el archivo "querys.log" en disco y le damos los permisos adecuados:
+touch /var/log/querys.log
+chown mysql:mysql /var/log/querys.log
+
+#Ejecutamos una sentencia en mysql 
+docker exec -i mysql bash < ejecutable_mysql.sh
