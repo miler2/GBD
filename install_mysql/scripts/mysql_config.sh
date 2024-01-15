81bc1ff3f53c52295212042bda6b86e0a27c139a
Ubuntu <ubuntu@ip-172-31-90-201.ec2.internal>
2024-01-15 08:57:27 +0000

    He conseguido conectarme como un usuario creado a Workbench

--- install_mysql/scripts/mysql_config.sh
+++ install_mysql/scripts/mysql_config.sh
@@ -15,3 +15,5 @@ mysql -u root -p$password_user_root <<< "DROP USER IF EXISTS '$usuario'@'%'"
 mysql -u root -p$password_user_root <<< "CREATE USER '$usuario'@'%' IDENTIFIED WITH mysql_native_password BY '$password_user_root'"
 mysql -u root -p$password_user_root <<< "ALTER USER '$usuario'@'%' IDENTIFIED WITH mysql_native_password BY '$password_user_root'"
 mysql -u root -p$password_user_root <<< "GRANT ALL PRIVILEGES On *.* TO '$usuario'@'%'"
+
+systemctl restart mysql
