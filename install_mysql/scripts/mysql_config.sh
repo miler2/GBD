8d829e3477efcaac84d92d33e049dfccd5a34a18
Ubuntu <ubuntu@ip-172-31-90-201.ec2.internal>
2024-02-05 10:48:49 +0000

    He modificado los archivos de instalación de mysql

--- install_mysql/scripts/mysql_config.sh
+++ install_mysql/scripts/mysql_config.sh
@@ -7,13 +7,11 @@ mysql -u root <<< "ALTER USER root@localhost IDENTIFIED WITH mysql_native_passwo
 #Creamos el usuario 'root'@'%'
 mysql -u root -p$password_user_root <<< "DROP USER IF EXISTS 'root'@'%'"
 mysql -u root -p$password_user_root <<< "CREATE USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '$password_user_root'"
-mysql -u root -p$password_user_root <<< "ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '$password_user_root'"
 mysql -u root -p$password_user_root <<< "GRANT ALL PRIVILEGES On *.* TO 'root'@'%'"
 
 #Creamos el usuario personal '[tu_usuario]'@'%'
 mysql -u root -p$password_user_root <<< "DROP USER IF EXISTS '$usuario'@'%'"
 mysql -u root -p$password_user_root <<< "CREATE USER '$usuario'@'%' IDENTIFIED WITH mysql_native_password BY '$password_user_root'"
-mysql -u root -p$password_user_root <<< "ALTER USER '$usuario'@'%' IDENTIFIED WITH mysql_native_password BY '$password_user_root'"
 mysql -u root -p$password_user_root <<< "GRANT ALL PRIVILEGES On *.* TO '$usuario'@'%'"
 
 systemctl restart mysql
