62ece9c9bbaaba9473a371b6a592406c9615250d
Ubuntu <ubuntu@ip-172-31-91-243.ec2.internal>
2024-02-19 08:45:31 +0000

    he arreglado algunos errores en el archivo terraform

--- ejercicios/U6P1_bases_de_datos_replicadas/terraform/main.tf
+++ ejercicios/U6P1_bases_de_datos_replicadas/terraform/main.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "security_group_mysql" {
 
 # Creamos las reglas de entrada del grupo de seguridad para mysql.
 resource "aws_security_group_rule" "ingres" {
-  security_group_id = aws_security_group.security_group.id
+  security_group_id = aws_security_group.security_group_mysql.id
   type              = "ingress"
 
   count       = length(var.puertos_security_group_mysql)
@@ -24,7 +24,7 @@ resource "aws_security_group_rule" "ingres" {
 
 # Creamos las reglas de salida del grupo de seguridad para mysql.
 resource "aws_security_group_rule" "egres" {
-  security_group_id = aws_security_group.security_group.id
+  security_group_id = aws_security_group.security_group_mysql.id
   type              = "egress"
 
   from_port   = 0
@@ -42,7 +42,7 @@ resource "aws_instance" "instancia_mysql_padre" {
   ami             = var.ami_id
   instance_type   = var.instance_type
   key_name        = var.key_name
-  security_groups = [aws_security_group.security_group.name]
+  security_groups = [aws_security_group.security_group_mysql.name]
 
   tags = {
     Name = var.nombre_instancia_mysql_padre
@@ -60,7 +60,7 @@ resource "aws_instance" "instancia_mysql_hijo" {
   ami             = var.ami_id
   instance_type   = var.instance_type
   key_name        = var.key_name
-  security_groups = [aws_security_group.security_group.name]
+  security_groups = [aws_security_group.security_group_mysql.name]
 
   tags = {
     Name = var.nombre_instancia_mysql_hijo
@@ -75,11 +75,11 @@ resource "aws_eip" "ip_elastica_mysql_hijo" {
 
 #--------------------------------------------------------------------------------------------------
 # Mostramos la IP pública de la máquina mysql padre
-output "elastic_ip" {
+output "elastic_ip_mysql_padre" {
   value = aws_eip.ip_elastica_mysql_padre.public_ip
 }
 
 # Mostramos la IP pública de la máquina mysql hijo
-output "elastic_ip" {
+output "elastic_ip_mysql_hijo" {
   value = aws_eip.ip_elastica_mysql_hijo.public_ip
 }
