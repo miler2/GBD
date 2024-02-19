8d4dbfe2a0faa7a569a58f6d51e4a43f216a6d67
Ubuntu <ubuntu@ip-172-31-94-200.ec2.internal>
2024-02-19 12:07:08 +0000

    He terminado la configuración del mysql padre

--- ejercicios/U6P1_bases_de_datos_replicadas/scripts/configuration_mysql_master.sh
+++ ejercicios/U6P1_bases_de_datos_replicadas/scripts/configuration_mysql_master.sh
@@ -22,13 +22,16 @@ mysql -u root -p$password_user_root <<< "DROP USER IF EXISTS '$usuario'@'%'"
 mysql -u root -p$password_user_root <<< "CREATE USER '$usuario'@'%' IDENTIFIED WITH mysql_native_password BY '$password_user_root'"
 mysql -u root -p$password_user_root <<< "GRANT ALL PRIVILEGES On *.* TO '$usuario'@'%'"
 
-# Creamos la base de datos liga, y otra más
+# Creamos la base de datos liga
 mysql -u root -p$password_user_root < ../../../bases_de_datos/liga.sql
+
+# Creamos otra base de datos extra (la base de datos que se ignorará)
 mysql -u root -p$password_user_root -e "DROP DATABASE IF EXISTS $base_de_datos_no_replica"
 mysql -u root -p$password_user_root -e "CREATE DATABASE $base_de_datos_no_replica"
 
 # Cambiamos los valores del archivo mysqld.cnf
-sed -i "s/127.0.0.1/$IP_MYSQL_HIJO/" /etc/mysql/mysql.conf.d/mysqld.cnf
+#sed -i "s/127.0.0.1/$IP_MYSQL_HIJO/" /etc/mysql/mysql.conf.d/mysqld.cnf        # Este no me funciona con la ip de la máquina, a si que lo dejo en 0.0.0.0
+sed -i "s/127.0.0.1/0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 sed -i "s/# server-id		= 1/server-id		= 1/" /etc/mysql/mysql.conf.d/mysqld.cnf
 sed -i "s:# log_bin                       = /var/log/mysql/mysql-bin.log:log_bin                       = /var/log/mysql/mysql-bin.log:" /etc/mysql/mysql.conf.d/mysqld.cnf
 sed -i "s/# binlog_do_db          = include_database_name/binlog_do_db          = $base_de_datos_no_replica/" /etc/mysql/mysql.conf.d/mysqld.cnf
@@ -39,3 +42,12 @@ systemctl restart mysql
 
 # Bloqueamos las tablas de la base de datos
 mysql -u root -p$password_user_root -e "FLUSH TABLES WITH READ LOCK"
+
+# Hacemos una copia de seguridad de la base de datos liga
+mysqldump -u root liga > dump.sql
+
+# Compartimos el archivo dump.sql con el otro ordenador (mysql_hijo)
+scp -i ~/GBD/ejercicios/U6P1_bases_de_datos_replicadas/scripts/labsuser_sad.pem dump.sql ubuntu@$IP_MYSQL_HIJO:/tmp/
+
+# Desbloqueamos las tablas de la base de datos
+mysql -u root -e "UNLOCK TABLES"
