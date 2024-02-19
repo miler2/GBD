72799c78892c675ab1330526acf619270271a00c
Ubuntu <ubuntu@ip-172-31-82-242.ec2.internal>
2024-02-19 11:31:19 +0000

    He avanzado un poco en la práctica y he creado un .gitignore

--- ejercicios/U6P1_bases_de_datos_replicadas/scripts/configuration_mysql_master.sh
+++ ejercicios/U6P1_bases_de_datos_replicadas/scripts/configuration_mysql_master.sh
@@ -11,8 +11,11 @@ apt install mysql-server -y
 
 source .env
 
+# Copiamos el archivo de configuración de mysql por defecto, para luego asegurarme de que los siguientes comandos funcionen
+cp ../config/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+
 # Modificamos los permisos del usuario root
-mysql -u root <<< "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY '$password_user_root'"
+#mysql -u root <<< "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY '$password_user_root'"
 
 # Creamos un usuario personal '[tu_usuario]'@'%'
 mysql -u root -p$password_user_root <<< "DROP USER IF EXISTS '$usuario'@'%'"
@@ -21,10 +24,18 @@ mysql -u root -p$password_user_root <<< "GRANT ALL PRIVILEGES On *.* TO '$usuari
 
 # Creamos la base de datos liga, y otra más
 mysql -u root -p$password_user_root < ../../../bases_de_datos/liga.sql
-mysql -u root -e "CREATE DATABASE base_de_datos_no_replica"
-
-# Copiamos el archivo de configuración de mysql por defecto, para luego asegurarme de que los siguientes comandos funcionen
-cp ../config/ /etc/mysql/mysql.conf.d/mysqld.cnf
+mysql -u root -p$password_user_root -e "DROP DATABASE IF EXISTS $base_de_datos_no_replica"
+mysql -u root -p$password_user_root -e "CREATE DATABASE $base_de_datos_no_replica"
 
 # Cambiamos los valores del archivo mysqld.cnf
-#sed -i "s/127.0.0.1/$MYSQL_PRIVATE_IP/" /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i "s/127.0.0.1/$IP_MYSQL_HIJO/" /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i "s/# server-id		= 1/server-id		= 1/" /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i "s:# log_bin                       = /var/log/mysql/mysql-bin.log:log_bin                       = /var/log/mysql/mysql-bin.log:" /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i "s/# binlog_do_db          = include_database_name/binlog_do_db          = $base_de_datos_no_replica/" /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i "s/# binlog_ignore_db      = include_database_name/binlog_ignore_db      = $base_de_datos_no_replica/" /etc/mysql/mysql.conf.d/mysqld.cnf
+
+# Reiniamos el servicio de mysql
+systemctl restart mysql
+
+# Bloqueamos las tablas de la base de datos
+mysql -u root -p$password_user_root -e "FLUSH TABLES WITH READ LOCK"
