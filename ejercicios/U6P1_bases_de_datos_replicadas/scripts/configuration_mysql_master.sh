55a3ac9fd69d94c9d2dbc78f6a223e6918e764b9
root <root@ip-172-31-85-215.ec2.internal>
2024-02-20 11:50:07 +0000

    He comprobado el funcionamiento manualmente del archivo de configuración mysql_padre, además de arreglar muchas cosas

--- ejercicios/U6P1_bases_de_datos_replicadas/scripts/configuration_mysql_master.sh
+++ ejercicios/U6P1_bases_de_datos_replicadas/scripts/configuration_mysql_master.sh
@@ -14,20 +14,17 @@ source .env
 # Copiamos el archivo de configuración de mysql por defecto, para luego asegurarme de que los siguientes comandos funcionen
 cp ../config/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 
-# Modificamos los permisos del usuario root
-#mysql -u root <<< "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY '$password_user_root'"
+# Creamos la base de datos liga
+mysql -u root < ../../../bases_de_datos/liga.sql
 
 # Creamos un usuario personal '[tu_usuario]'@'%'
-mysql -u root -p$password_user_root <<< "DROP USER IF EXISTS '$usuario'@'%'"
-mysql -u root -p$password_user_root <<< "CREATE USER '$usuario'@'%' IDENTIFIED WITH mysql_native_password BY '$password_user_root'"
-mysql -u root -p$password_user_root <<< "GRANT ALL PRIVILEGES On *.* TO '$usuario'@'%'"
-
-# Creamos la base de datos liga
-mysql -u root -p$password_user_root < ../../../bases_de_datos/liga.sql
+mysql -u root -e "DROP USER IF EXISTS '$usuario'@'%'"
+mysql -u root -e "CREATE USER '$usuario'@'%' IDENTIFIED WITH mysql_native_password BY '$password_user_root'"
+mysql -u root -e "GRANT ALL PRIVILEGES On *.* TO '$usuario'@'%'"
 
 # Creamos otra base de datos extra (la base de datos que se ignorará)
-mysql -u root -p$password_user_root -e "DROP DATABASE IF EXISTS $base_de_datos_no_replica"
-mysql -u root -p$password_user_root -e "CREATE DATABASE $base_de_datos_no_replica"
+mysql -u root -e "DROP DATABASE IF EXISTS $base_de_datos_no_replica"
+mysql -u root -e "CREATE DATABASE $base_de_datos_no_replica"
 
 # Cambiamos los valores del archivo mysqld.cnf
 #sed -i "s/127.0.0.1/$IP_MYSQL_HIJO/" /etc/mysql/mysql.conf.d/mysqld.cnf        # Este no me funciona con la ip de la máquina, a si que lo dejo en 0.0.0.0
@@ -41,13 +38,13 @@ sed -i "s/# binlog_ignore_db	= include_database_name/binlog_ignore_db	= $base_de
 systemctl restart mysql
 
 # Bloqueamos las tablas de la base de datos
-mysql -u root -p$password_user_root -e "FLUSH TABLES WITH READ LOCK"
+mysql -u root -e "FLUSH TABLES WITH READ LOCK"
 
 # Hacemos una copia de seguridad de la base de datos liga
-mysqldump -u root -p$password_user_root liga > dump.sql
+mysqldump -u root liga > dump.sql
 
 # Compartimos el archivo dump.sql con el otro ordenador (mysql_hijo)
-#scp -i labsuser_sad.pem dump.sql ubuntu@$IP_MYSQL_HIJO:/tmp/
+scp -i labsuser_sad.pem dump.sql ubuntu@$IP_MYSQL_HIJO:/tmp/
 
 # Desbloqueamos las tablas de la base de datos
-mysql -u root -p$password_user_root -e "UNLOCK TABLES"
+mysql -u root -e "UNLOCK TABLES"
