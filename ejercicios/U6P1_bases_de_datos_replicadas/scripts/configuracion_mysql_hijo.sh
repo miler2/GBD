655c88036b402236bf2f4ad899e4110c77c1364f
Ubuntu <ubuntu@ip-172-31-94-200.ec2.internal>
2024-02-20 10:42:20 +0000

    He terminado la práctica

--- ejercicios/U6P1_bases_de_datos_replicadas/scripts/configuracion_mysql_hijo.sh
+++ ejercicios/U6P1_bases_de_datos_replicadas/scripts/configuracion_mysql_hijo.sh
@@ -11,14 +11,18 @@ apt install mysql-server -y
 
 source .env
 
+# Copiamos el archivo de configuración de mysql por defecto, para luego asegurarme de que los siguientes comandos funcionen
+cp ../config/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+
 # Modificamos los permisos del usuario root
-mysql -u root <<< "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY '$password_user_root'"
+#mysql -u root <<< "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY '$password_user_root'"
 
 # Creamos la base de datos liga (sin datos)
 mysql -u root -p$password_user_root -e "CREATE DATABASE if not exists liga"
 
 # Importamos los valores de la base de datos liga
-mysql -u root -p$password_user_root liga < /tmp/dump.sql
+#mysql -u root -p$password_user_root liga < /tmp/dump.sql
+mysql -u root -p$password_user_root liga < dump.sql
 
 # Cambiamos los valores del archivo mysqld.cnf
 sed -i "s/# server-id		= 1/server-id		= 2/" /etc/mysql/mysql.conf.d/mysqld.cnf
@@ -30,5 +34,12 @@ echo "relay-log = /var/log/mysql/mysql-relay-bin.log" >> /etc/mysql/mysql.conf.d
 systemctl restart mysql
 
 # Cambiamos la configuración de la replicación de mysql
-mysql -u root -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='master',SOURCE_USER='usuario_replicacion',SOURCE_PASSWORD='contraseña',SOURCE_LOG_FILE='mysql-bin.000001',SOURCE_LOG_POS=157"
+mysql -u root -p$password_user_root -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='master',SOURCE_USER='usuario_replicacion',SOURCE_PASSWORD='contraseña',SOURCE_LOG_FILE='mysql-bin.000001',SOURCE_LOG_POS=157"
+
+# Arrancamos la replicacion
+mysql -u root -p$password_user_root -e "START REPLICA"
+
+# Mostramos un mensaje de comprobación
+mysql -u root -p$password_user_root -e "SHOW REPLICA STATUS\G" | grep State
+
 
