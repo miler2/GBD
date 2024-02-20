425548a51951506d63903b6d5390f6b8e10ac162
Ubuntu <ubuntu@ip-172-31-84-215.ec2.internal>
2024-02-20 11:59:13 +0000

    He conseguido que funcionesudo su!

--- ejercicios/U6P1_bases_de_datos_replicadas/scripts/configuracion_mysql_hijo.sh
+++ ejercicios/U6P1_bases_de_datos_replicadas/scripts/configuracion_mysql_hijo.sh
@@ -14,15 +14,12 @@ source .env
 # Copiamos el archivo de configuración de mysql por defecto, para luego asegurarme de que los siguientes comandos funcionen
 cp ../config/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 
-# Modificamos los permisos del usuario root
-#mysql -u root <<< "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY '$password_user_root'"
-
 # Creamos la base de datos liga (sin datos)
-mysql -u root -p$password_user_root -e "CREATE DATABASE if not exists liga"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS liga"
 
 # Importamos los valores de la base de datos liga
-#mysql -u root -p$password_user_root liga < /tmp/dump.sql
-mysql -u root -p$password_user_root liga < dump.sql
+#mysql -u root liga < /tmp/dump.sql
+mysql liga < /tmp/dump.sql
 
 # Cambiamos los valores del archivo mysqld.cnf
 sed -i "s/# server-id		= 1/server-id		= 2/" /etc/mysql/mysql.conf.d/mysqld.cnf
@@ -34,12 +31,12 @@ echo "relay-log = /var/log/mysql/mysql-relay-bin.log" >> /etc/mysql/mysql.conf.d
 systemctl restart mysql
 
 # Cambiamos la configuración de la replicación de mysql
-mysql -u root -p$password_user_root -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='$IP_MYSQL_PADRE',SOURCE_USER='$usuario',SOURCE_PASSWORD='$password_user_root',SOURCE_LOG_FILE='mysql-bin.000001',SOURCE_LOG_POS=157"
+mysql -u root -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='$IP_MYSQL_PADRE',SOURCE_USER='$usuario',SOURCE_PASSWORD='$password_user_root',SOURCE_LOG_FILE='mysql-bin.000001',SOURCE_LOG_POS=157"
 
 # Arrancamos la replicacion
-mysql -u root -p$password_user_root -e "START REPLICA"
+mysql -u root -e "START REPLICA"
 
 # Mostramos un mensaje de comprobación
-mysql -u root -p$password_user_root -e "SHOW REPLICA STATUS\G" | grep State
+mysql -u root -e "SHOW REPLICA STATUS\G" | grep State
 
 
